5f76395acfdd5262d9b6e7fd39be6558c651de12
Abdelhaq Aouan <133820936+abdelhaq24Git@users.noreply.github.com>
2023-09-11 23:44:17 +0100

    Update bassin.py

--- samples/bassins/bassin.py
+++ samples/bassins/bassin.py
@@ -24,14 +24,10 @@ DEFAULT_LOGS_DIR = os.path.join(ROOT_DIR, "logs")
 
 class BassinConfig(Config):
     NAME = "bassin"
-    IMAGES_PER_GPU = 4  # Adjust based on available GPU memory
-    BATCH_SIZE=4
+    IMAGES_PER_GPU = 1  # Adjust based on available GPU memory
     NUM_CLASSES = 1 + 1  # Background + basin
-    STEPS_PER_EPOCH = 142  # Assuming 143 images and batch size of 1
+    STEPS_PER_EPOCH = 131  # Assuming 143 images and batch size of 1
     DETECTION_MIN_CONFIDENCE = 0.6 # Adjust as needed
-    USE_MINI_MASK=False
-    IMAGE_MIN_DIM=250
-    IMAGE_MAX_DIM=256
 class BassinDataset(utils.Dataset):
   def load_basin(self, dataset_dir, subset):
     """Load a subset of the Basin dataset.
@@ -39,68 +35,66 @@ class BassinDataset(utils.Dataset):
     subset: Subset to load: train or val
     """
     # Add classes. You might have different class names and IDs.
-    self.add_class("basin", 1, "irrigation_basin")
+           self.add_class("bassin", 1, "irrigation_basin")
 
     # Train or validation dataset?
-    assert subset in ["train", "val"]
-    dataset_dir = os.path.join(dataset_dir, subset)
+           assert subset in ["train", "val"]
+           image_dir = os.path.join(dataset_dir,subset,subset + "_images")  # Use "_images" suffix for images
+           mask_dir = os.path.join(dataset_dir,subset,subset + "_masks")    # Use "_masks" suffix for masks
 
-    # Load annotations from your JSON file
-    annotations = json.load(open(os.path.join(dataset_dir, "annotations.json")))
+    # Get a list of image file names
+           image_files=os.listdir(image_dir)
 
-    # Filter out annotations without bounding box information
-    annotations = [a for a in annotations if 'bbox' in a]
+           for image_file in image_files:
+               if image_file.endswith(".tif"):
+                # Construct the full path to the image file
+                   image_fp_full = os.path.join(image_dir, image_file)
 
-    # Add images
-    for annotation in annotations:
-        # Extract bounding box coordinates
-        bbox = annotation['bbox']
-        x, y, width, height = bbox
+                # Construct the corresponding mask file path based on the image file name
+                   mask_file = image_file.replace(".tif", ".tif")  # Adjust the mask file extension if needed
+                   mask_fp = os.path.join(mask_dir, mask_file)
 
-        # Load image size from image file
-        image_path = os.path.join(dataset_dir, "train_images", annotation['image_id'])
-        image = skimage.io.imread(image_path)
-        height, width = image.shape[:2]
+                  # Load image size
+                  image = skimage.io.imread(image_fp_full)
+                  height, width = image.shape[:2]
 
-        self.add_image(
-            "basin",
-            image_id=annotation['image_id'],  # use file name as a unique image id
-            path=image_path,
-            width=width, height=height,
-            bbox=bbox)  # Pass the bounding box information to the dataset
-  def load_mask(self, image_id):
-    """Generate instance masks for an image.
-    Returns:
-    masks: A bool array of shape [height, width, instance count] with
-        one mask per instance.
-    class_ids: a 1D array of class IDs of the instance masks.
-    """
-    image_info = self.image_info[image_id]
-    
-    # Check the source of the image
-    if image_info["source"] != "bassin":
-        return super(self.__class__, self).load_mask(image_id)
+                  self.add_image(
+                      "bassin",
+                      image_id=image_file[:-4],  # Remove the last 4 characters (".tif") to get image_id
+                      path=image_fp_full,
+                      width=width,
+                      height=height,
+                      mask_path=mask_fp)
+               
+    def load_mask(self, image_id):
+    # Load the multi-class mask
+        mask_path = os.path.join(self.mask_dir, self.image_info[image_id]['id'] + '.tif')
+        mask = skimage.io.imread(mask_path)
 
-    # Load annotations for the image
-    annotations = self.image_info[image_id]["annotations"]
-    num_instances = len(annotations)
+    # Calculate the number of basins
+        num_basins = np.max(mask)
 
-    # Initialize mask array
-    mask = np.zeros([image_info["height"], image_info["width"], num_instances],
-                    dtype=np.uint8)
+    # Create an empty array for masks and class IDs
+        masks = []
+        class_ids = []
 
-    for i, annotation in enumerate(annotations):
-        x, y, w, h = annotation["bbox"]  # Extract bbox coordinates
-        x = int(x)
-        y = int(y)
-        w = int(w)
-        h = int(h)
+    # Convert each basin into a binary mask
+        for basin_id in range(1, num_basins + 1):
+            binary_mask = (mask == basin_id).astype(np.uint8)
+            masks.append(binary_mask)
+            class_ids.append(1)  # Class ID for basins
 
-        # Set pixel values inside the bounding box to 1
-        mask[y:y+h, x:x+w, i] = 1
+    # Stack the binary masks along the third axis to create a 3D mask array
+        if masks:
+            masks = np.stack(masks, axis=-1).astype(np.bool)
+            class_ids = np.array(class_ids, dtype=np.int32)
+        else:
+            # If there are no basins, create empty arrays
+            masks = np.zeros((mask.shape[0], mask.shape[1], 0), dtype=np.bool)
+            class_ids = np.zeros((0,), dtype=np.int32)
 
-    # Return masks and class IDs
-    return mask.astype(np.bool), np.ones([num_instances], dtype=np.int32)
+        return masks, class_ids
+
   def image_reference(self, image_id):
     """Return the path of the image."""
     info = self.image_info[image_id]
@@ -175,16 +169,16 @@ def main():
     # Load weights
     print("Loading weights ", args.weights)
     if args.command == "train":
-        model.load_weights(args.weights, by_name=True,exclude=["mrcnn_class_logits", "mrcnn_bbox_fc", "mrcnn_bbox", "mrcnn_mask"])
+        model.load_weights(args.weights, by_name=True)
     else:
         model.load_weights(args.weights, by_name=True)
 
     if args.command == "train":
-        dataset_train = BassinDataset()
+        dataset_train = BasinDataset()
         dataset_train.load_basin(args.dataset, "train")
         dataset_train.prepare()
 
-        dataset_val = BassinDataset()
+        dataset_val = BasinDataset()
         dataset_val.load_basin(args.dataset, "val")
         dataset_val.prepare()
 
